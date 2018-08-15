9121a29ae7d5ed516efc822d0410097013928ca9
sw00 <exec@sett.sh>
2018-08-15 21:05:24 +0200

    Implement Intervals object
    - Accepts a collection of TimeWarriorIntervals
    - Can tally up by per days

--- billable.py
+++ billable.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3
+import datetime
+
+
+class Intervals(object):
+    def __init__(self, list_of_intervals):
+        self._intervals = dict()
+
+        for interval in list_of_intervals:
+            records = self._intervals.get(interval.get_start().date(), [])
+            records.append(interval.get_duration())
+            self._intervals[interval.get_start().date()] = records
+
+    def totals_by_days(self):
+        """Returns total duration per day."""
+        return {
+            day: sum(records, datetime.timedelta())
+            for day, records in self._intervals.items()
+        }
--- test_billable.py
+++ test_billable.py
@@ -1,1 +1,43 @@
-#!/usr/bin/env python3
+import unittest
+from datetime import date, datetime, timedelta
+from random import randint
+
+from timewreport.interval import TimeWarriorInterval
+
+import billable
+
+DT_FORMAT = "%Y%m%dT%H%M%SZ"
+
+
+class BillableTest(unittest.TestCase):
+    def test_totals_by_days_should_sum_multiple_intervals_on_same_day(self):
+        any_day = datetime.today().replace(day=randint(1, 28), month=randint(1, 12))
+
+        start_a = any_day.replace(hour=12)
+        end_a = start_a + timedelta(0, 60)
+
+        start_b = end_a + timedelta(0, 60)
+        end_b = start_b + timedelta(0, 60)
+
+        interval_a = TimeWarriorInterval(
+            start_a.strftime(DT_FORMAT),
+            end_a.strftime(DT_FORMAT),
+            ["A long, descriptive tag", "single-word-tag", "tag123"],
+        )
+        interval_b = TimeWarriorInterval(
+            start_b.strftime(DT_FORMAT),
+            end_b.strftime(DT_FORMAT),
+            ["A long, descriptive tag", "single-word-tag", "tag123"],
+        )
+
+        intervals = billable.Intervals([interval_a, interval_b])
+        days = intervals.totals_by_days()
+        self.assertIn(any_day.date(), days)
+        self.assertEqual(
+            days.get(any_day.date()),
+            (interval_a.get_duration() + interval_b.get_duration()),
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
