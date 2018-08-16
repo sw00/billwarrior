6dbbd6cdd10898a3dc564240ea233797b2e92eeb
Sett Wai <exec@sett.sh>
2018-08-16 20:59:07 +0200

    Add validation to Day
    - ensure intervals given are all on same day

--- billwarrior/values.py
+++ billwarrior/values.py
@@ -3,6 +3,9 @@ from datetime import timedelta
 
 class Day(object):
     def __init__(self, intervals):
+        if len(set([interval.get_date() for interval in intervals])) > 1:
+            raise ValueError
+
         self.__intervals = intervals
 
     def total_duration(self):
--- tests/test_values.py
+++ tests/test_values.py
@@ -7,11 +7,23 @@ from billwarrior.values import Day
 
 class DayTest(unittest.TestCase):
     def test_total_should_sum_entries_together(self):
-        intervals = [(tests.give_interval()), tests.give_interval()]
+        same_day = datetime.today()
+        intervals = [(tests.give_interval(same_day)),
+                tests.give_interval(same_day)]
         day = Day(intervals)
 
         self.assertEqual(
-                day.total_duration(),
-                sum([interval.get_duration() for interval in intervals],
-                    timedelta())
-                )
+            day.total_duration(),
+            sum([interval.get_duration() for interval in intervals], timedelta()),
+        )
+
+    def test_exception_raised_when_intervals_have_different_start_dates(self):
+        a = tests.give_interval()
+        b = tests.give_interval(a.get_date() + timedelta(days=7))
+
+        with self.assertRaises(ValueError):
+            Day([a, b])
+
+    @unittest.skip
+    def test_date_should_return_date_object_representing_day(self):
+        pass
