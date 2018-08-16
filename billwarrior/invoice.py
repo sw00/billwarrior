26cb281e9492c8292f9d1486b57c5a8e97cef717
Sett Wai <exec@sett.sh>
2018-08-16 13:51:46 +0200

    Code formatting

--- billwarrior/invoice.py
+++ billwarrior/invoice.py
@@ -1,10 +1,12 @@
 class Invoice(object):
     def __init__(self, intervals):
-        self.__categories = [tag for interval in intervals for tag in
-                interval.get_tags()]
+        self.__categories = [
+            tag for interval in intervals for tag in interval.get_tags()
+        ]
 
     def categories(self):
         return self.__categories
+
 
 class LineItem(object):
     def __init__(self, day, duration, unit_price):
@@ -14,19 +16,19 @@ class LineItem(object):
 
     @property
     def date(self):
-        return self.__day.strftime('%B %d, %Y')
+        return self.__day.strftime("%B %d, %Y")
 
     @property
     def duration(self):
         totsec = self.__duration.total_seconds()
-        h = totsec//3600
-        m = (totsec%3600) // 60
-        s = (totsec%3600)%60
+        h = totsec // 3600
+        m = (totsec % 3600) // 60
+        s = (totsec % 3600) % 60
 
-        total = h + (m/60) + (s/6000)
+        total = h + (m / 60) + (s / 6000)
 
-        return '{:.2f}'.format(total)
+        return "{:.2f}".format(total)
 
     @property
     def unit_price(self):
-        return '{:.2f}'.format(self.__unit_price)
+        return "{:.2f}".format(self.__unit_price)
--- tests/test_invoice.py
+++ tests/test_invoice.py
@@ -11,15 +11,15 @@ from billwarrior.invoice import Invoice, LineItem
 class InvoiceTest(unittest.TestCase):
     def test_categories_should_return_list_of_tags(self):
         a, b, c = (
-                tests.give_interval(tags=['meeting']),
-                tests.give_interval(tags=['pingpong']),
-                tests.give_interval(tags=['coding']),
-                )
+            tests.give_interval(tags=["meeting"]),
+            tests.give_interval(tags=["pingpong"]),
+            tests.give_interval(tags=["coding"]),
+        )
 
-        invoice = Invoice([a,b,c])
+        invoice = Invoice([a, b, c])
         categories = invoice.categories()
 
-        self.assertCountEqual(['meeting', 'pingpong', 'coding'], categories)
+        self.assertCountEqual(["meeting", "pingpong", "coding"], categories)
 
 
 class LineItemTest(unittest.TestCase):
@@ -27,20 +27,25 @@ class LineItemTest(unittest.TestCase):
         day = datetime.today()
         line_item = LineItem(day, None, None)
 
-        self.assertEqual(line_item.date, day.strftime('%B %d, %Y'))
+        self.assertEqual(line_item.date, day.strftime("%B %d, %Y"))
 
     def test_duration_should_display_decimal_value_of_hours_worked(self):
-        duration = timedelta(hours=randint(0,8), minutes=randint(0,59),
-                seconds=randint(0,59))
+        duration = timedelta(
+            hours=randint(0, 8), minutes=randint(0, 59), seconds=randint(0, 59)
+        )
         line_item = LineItem(datetime.today(), duration, None)
 
         totsec = duration.total_seconds()
-        total = totsec//3600 + (((totsec%3600)//60)/60) + (((totsec%3600)%60)/6000)
-        
-        self.assertEqual(line_item.duration, '{:.2f}'.format(total))
-    
+        total = (
+            totsec // 3600
+            + (((totsec % 3600) // 60) / 60)
+            + (((totsec % 3600) % 60) / 6000)
+        )
+
+        self.assertEqual(line_item.duration, "{:.2f}".format(total))
+
     def test_unit_price_should_be_displayed_as_is(self):
-        unit_price = randint(0, 200) + randint(0,99) * 0.1
+        unit_price = randint(0, 200) + randint(0, 99) * 0.1
         line_item = LineItem(datetime.today(), timedelta(), unit_price)
 
-        self.assertEqual(line_item.unit_price, '{:.2f}'.format(unit_price))
+        self.assertEqual(line_item.unit_price, "{:.2f}".format(unit_price))
