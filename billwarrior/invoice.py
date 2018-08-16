7cf38084ab458e049eb67d57f45799e948ab7c45
Sett Wai <exec@sett.sh>
2018-08-16 13:39:34 +0200

    Make LineItem display hours as decimals

--- billwarrior/invoice.py
+++ billwarrior/invoice.py
@@ -9,7 +9,20 @@ class Invoice(object):
 class LineItem(object):
     def __init__(self, day, duration, rate):
         self.__day = day.date()
+        self.__duration = duration
 
     @property
     def date(self):
         return self.__day.strftime('%B %d, %Y')
+
+    @property
+    def duration(self):
+        totsec = self.__duration.total_seconds()
+        h = totsec//3600
+        m = (totsec%3600) // 60
+        s = (totsec%3600)%60
+
+        total = h + (m/60) + (s/6000)
+
+        return '{:.2f}'.format(total)
+
--- tests/test_invoice.py
+++ tests/test_invoice.py
@@ -1,5 +1,6 @@
 import unittest
-from datetime import datetime
+from datetime import datetime, timedelta
+from random import randint
 
 from timewreport.interval import TimeWarriorInterval
 
@@ -24,7 +25,19 @@ class InvoiceTest(unittest.TestCase):
 class LineItemTest(unittest.TestCase):
     def test_date_should_display_month_day_year_correctly_in_latex(self):
         day = datetime.today()
-        line_item = LineItem(day, 0, 0)
+        line_item = LineItem(day, None, None)
 
         self.assertEqual(line_item.date, day.strftime('%B %d, %Y'))
+
+    def test_duration_should_display_decimal_value_of_hours_worked(self):
+        duration = timedelta(hours=randint(0,8), minutes=randint(0,59),
+                seconds=randint(0,59))
+        line_item = LineItem(datetime.today(), duration, None)
+
+        totsec = duration.total_seconds()
+        total = totsec//3600 + (((totsec%3600)//60)/60) + (((totsec%3600)%60)/6000)
+        
+        self.assertEqual(line_item.duration, '{:.2f}'.format(total))
+
+
 
