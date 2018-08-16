80ea39fec4fa86a21e1f1e7faa46ce86963bdc74
Sett Wai <exec@sett.sh>
2018-08-16 11:10:58 +0200

    Implement adding of interval to Tag

--- billwarrior/tag.py
+++ billwarrior/tag.py
@@ -1,15 +1,19 @@
 class Tag(object):
     def __init__(self, name, intervals):
         self._name = name
-        self._intervals = intervals
+        self.__record_collection = dict()
+        self.__sort_into_day_entries(intervals)
 
     def records(self):
-        record_collection = dict()
-        for interval in self._intervals:
-            entry = record_collection.get(interval.get_start().date(), [])
+        return self.__record_collection
+
+    def add(self, interval):
+        interval.get_start().date()
+        entry = self.__record_collection.get(interval.get_start().date(), [])
+        entry.append(interval)
+
+    def __sort_into_day_entries(self, intervals):
+        for interval in intervals:
+            entry = self.__record_collection.get(interval.get_start().date(), [])
             entry.append(interval)
-            record_collection[interval.get_start().date()] = entry
-
-        return record_collection
-        
-
+            self.__record_collection[interval.get_start().date()] = entry
--- tests/test_tag.py
+++ tests/test_tag.py
@@ -7,11 +7,11 @@ from billwarrior import tag
 class TagTest(unittest.TestCase):
     def test_records_should_return_records_grouped_by_day_for_tag(self):
         a, b = (
-            tests.give_interval(tags=["coding"]),
-            tests.give_interval(tags=["coding"]),
+            tests.give_interval(tags=['coding']),
+            tests.give_interval(tags=['coding']),
         )
 
-        coding_tag = tag.Tag("coding", [a, b])
+        coding_tag = tag.Tag('coding', [a, b])
         records = coding_tag.records()
 
         self.assertIn(a.get_date().date(), records.keys())
@@ -19,3 +19,17 @@ class TagTest(unittest.TestCase):
         self.assertIn(a, records.get(a.get_date().date()))
         self.assertIn(b, records.get(b.get_date().date()))
         
+    def test_add_should_add_record_to_day_under_tag(self):
+        a, b = (
+            tests.give_interval(tags=['coding']),
+            tests.give_interval(tags=['coding']),
+        )
+
+        c = tests.give_interval(a.get_date(), tags=['coding'])
+
+        coding_tag = tag.Tag('coding', [a, b])
+        coding_tag.add(c)
+        records = coding_tag.records()
+
+        self.assertIn(c, records.get(a.get_date().date()))
+
